bb8bf7893a8efe883ebb0a71012560dcafa7dcc4
Arto Olli <arto.olli@live.com>
2022-12-23 23:55:36 -0800

    Add Finglish layout with custom_config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,14 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_LAYER_BASE \
+&kp Q,             &kp O,             &kp M,             &kp B,             &kp COMMA,         &kp DOT,           &kp F,             &kp W,             &kp D,             &kp SQT,           \
+U_MT(LGUI, N),     U_MT(LALT, S),     U_MT(LCTRL, R),    U_MT(LSHFT, T),    &kp G,             &kp Y,             U_MT(LSHFT, H),    U_MT(LCTRL, E),    U_MT(LALT, A),     U_MT(LGUI, I),     \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp K,             &kp C,             &kp V,             &kp J,             &kp L,             &kp U,             U_MT(RALT, P),     U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_FUN, DEL),  U_LT(U_NUM, BSPC), U_LT(U_SYM, RET),  U_LT(U_MOUSE, TAB),U_LT(U_NAV, SPC),  U_LT(U_MEDIA, ESC),U_NP,              U_NP
+
+#define MIRYOKU_ALTERNATIVES_TAP_WORKMAN \
+&kp Q,             &kp O,             &kp M,             &kp B,             &kp COMMA,         &kp DOT,           &kp F,             &kp W,             &kp D,             &kp SQT,           \
+&kp N,             &kp S,             &kp R,             &kp T,             &kp G,             &kp Y,             &kp H,             &kp E,             &kp A,             &kp I,             \
+&kp Z,             &kp X,             &kp K,             &kp C,             &kp V,             &kp J,             &kp L,             &kp U,             &kp P,             &kp SLASH,         \
+U_NP,              U_NP,              &kp ESC,           &kp SPC,           &kp TAB,           &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
